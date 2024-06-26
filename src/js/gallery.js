3d162d6e3e2478fba68f6fd08b50e8842587c757
Symeon Melnychuk <m.simeon998@gmail.com>
2024-06-26 20:07:31 +0300

    add adaptive gallery slider

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,18 +1,35 @@
 import 'swiper/css';
 
 new Swiper('#swiper1', {
-  direction: 'horizontal',
-  centeredSlides: true,
-  // centeredSlidesBounds: true,
-
-  spaceBetween: 25,
+  effect: 'coverflow',
   loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
 
-  slidesPerView: 3,
+  breakpoints: {
+    375: {
+      slidesPerView: 'auto',
+      spaceBetween: 25,
+    },
 
+    1440: {
+      slidesPerView: 'auto',
+    },
+  },
+
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 25,
+    depth: 140,
+    modifier: 0.4,
+    slideShadows: false,
+
+    scale: 0.5,
+  },
   navigation: {
     nextEl: '#swiper1 .swiper-button-next',
     prevEl: '#swiper1 .swiper-button-prev',
-    // clickable: true,
+    clickable: true,
   },
 });
